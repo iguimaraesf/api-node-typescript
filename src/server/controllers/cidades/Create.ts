ff80da55341c0c6b886a45471e420a5d83dff6a8
Ivan Guimarães Farias <iguimaraesf@gmail.com>
2023-03-05 18:15:38 -0300

    feat: validação dos dados

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,25 +1,33 @@
 import { Request, Response } from 'express'
+import { StatusCodes } from 'http-status-codes'
 import * as yup from 'yup'
 
 interface InterfaceCidade {
     nome: string
+    estado: string
 }
 
 const validacao: yup.ObjectSchema<InterfaceCidade> = yup.object().shape({
     nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
 })
 
 export const create = async (req: Request<{}, {}, InterfaceCidade>, res: Response) => {
     const data = req.body
     let validatedData: InterfaceCidade | undefined = undefined
     try {
-        validatedData = await validacao.validate(data)
-    } catch (error) {
-        const yupError = error as yup.ValidationError
-        return res.json({
-            errors: {
-                default: yupError.message
-            }
+        validatedData = await validacao.validate(data, { abortEarly: false })
+    } catch (err) {
+        const yupError = err as yup.ValidationError
+        const validationErrors: Record<string, string> = {}
+
+        yupError.inner.forEach(error => {
+            if (!error.path) return
+
+            validationErrors[error.path] = error.message
+        })
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: validationErrors
         })
     }
     console.log(validatedData)
